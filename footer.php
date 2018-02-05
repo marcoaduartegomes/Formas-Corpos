c3978f7f1020b415e6c2964055b118ee0a5365f5
marcoaduartegomes <marcoaduartegomes@gmail.com>
2018-02-05 14:49:20 -0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Formas e Corpos</title>
+    <link rel="icon" href="imagens/favicon.png">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="headerFooter.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+<footer>
+                 <div class="container" >
+            
+                    <div class="row">
+                
+                        <div class="col-md-6">
+                             <div class="FormatacaoParagrafo">
+                              <p>Copyright &copy; 2017  - Formas e Corpos</p>
+                            </div>
+                         </div>
+                      <div class="row">
+                          <div class="col-md-6">
+                            <div class="FormatacaoImagem">
+                              <p> Projetado por
+                              <img class="imagem2" src="Logo Serra.png">
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+            
+                 </div>
+            </footer>
+          
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+  </body>
+</html>
